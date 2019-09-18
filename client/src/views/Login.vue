9c02d75f181b5a77ce3c937020553f6c2c26fd55
Zach Tyler <zach92tyler@gmail.com>
2019-09-18 16:14:25 -0600

    fixed <b> MAJOR</b> issue with login and register buttons

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -3,13 +3,13 @@
     <form v-if="loginForm" @submit.prevent="loginUser">
       <input type="email" v-model="creds.email" placeholder="email" />
       <input type="password" v-model="creds.password" placeholder="password" />
-      <button class="btn btn-success" type="submit">Login</button>
+      <button class="btn btn-dark btn-outline-light" type="submit">Login</button>
     </form>
     <form v-else @submit.prevent="register">
       <input type="text" v-model="newUser.name" placeholder="name" />
       <input type="email" v-model="newUser.email" placeholder="email" />
       <input type="password" v-model="newUser.password" placeholder="password" />
-      <button class="btn btn-warning" type="submit">Create Account</button>
+      <button class="btn btn-outline-dark" type="submit">Create Account</button>
     </form>
     <div class="action" @click="loginForm = !loginForm">
       <p v-if="loginForm">No account? Click here to Register</p>
